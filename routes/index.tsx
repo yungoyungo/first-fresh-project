b6eddc6123b1a4c9c7dd9db449a07f02b3fa9aba
Yungo <29724679+yungoyungo@users.noreply.github.com>
2024-10-07 22:21:05 +0900

    fix: デプロイこけたのでコメント消してみる

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div class="h-dvh mx-auto my-auto flex flex-col items-center justify-center space-y-8 bg-gray-700 font-Dot text-gray-100">
       <h1 class="text-2xl">アカウント分けすぎた.com</h1>
       <div class="py-4 px-4 flex flex-col items-center justify-center space-y-4">
-        {/* <div>ヒｗｉヒヒｅｒｓ</div> */}
         <Link url="https://x.com/sslloowwllyy" text="キュウリンゴ" pColor="blue-300" />
         <Link url="https://x.com/pasocon_suko" text="ｗｅｂ屋" pColor="teal-300" />
         <Link url="https://x.com/slow_slow_down_" text="車" pColor="orang-400" />
