2594fb58bdc0515ca52acc364ef9cb75590dd939
Yungo <29724679+yungoyungo@users.noreply.github.com>
2024-11-25 23:29:19 +0900

    feat: リンク追加,deno fresh upgrade

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,12 +7,16 @@ export default function Home() {
     <div class="h-dvh m-auto flex flex-col items-center justify-center bg-gray-700 font-Dot text-gray-100">
       <h1 class="text-2xl">爆速.com</h1>
       <span class=" border-white hover:rotate-[360deg] transition-transform duration-700" style="font-size:8rem">〄</span>
-      こ、これはSOS団じゃなくてJISマークなんだからねっ！
+      <div>こ、これはSOS団じゃなくて</div>
+      <div>U+3004(旧JISマーク)なんだからねっ！</div>
       <div class="py-4 px-4 flex flex-col items-center justify-center space-y-2">
-        <Link url="https://x.com/sslloowwllyy" text="キュウリンゴ" pColor="blue-300" />
-        <Link url="https://x.com/pasocon_suko" text="ｗｅｂ屋" pColor="teal-300" />
-        <Link url="https://x.com/slow_slow_down_" text="車" pColor="orang-400" />
-        <Link url="https://x.com/z_sh_rc" text="ゲーム" pColor="blue-300" />
+        <Link url="https://x.com/sslloowwllyy" text="キュウリンゴ垢" pColor="blue-300" />
+        <Link url="https://x.com/pasocon_suko" text="ｗｅｂ屋垢" pColor="teal-300" />
+        <Link url="https://x.com/slow_slow_down_" text="車垢" pColor="orang-400" />
+        <Link url="https://x.com/z_sh_rc" text="ゲーム垢" pColor="blue-300" />
+        <Link url="https://note.com/sslloowwllyy/" text="note" pColor="blue-300" />
+        <Link url="https://photohito.com/user/photo/166054/" text="photohito" pColor="blue-300" />
+        <Link url="https://open.spotify.com/playlist/1ccRI6DwKUiXnanK6A5WXO?si=66bcdd1528d8411e" text="私を構成するなんとか" pColor="blue-300" />
       </div>
     </div>
   );
