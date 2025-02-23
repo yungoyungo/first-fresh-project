376aabafe1762a7c447426c3064763cd9e5f07e4
Yungo <29724679+yungoyungo@users.noreply.github.com>
2025-02-23 09:18:59 +0900

    feat: Googleタグ テスト導入

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,14 @@ export default function App({ Component }: PageProps) {
         <title>ユ</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EBJ2Q74TDC"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EBJ2Q74TDC');
+        </script>
       </head>
       <body class="bg-gray-700 font-Dot text-gray-100">
         <Component />
