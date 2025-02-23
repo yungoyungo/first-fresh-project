882b6fdc73c069087a0aa6760c4e8ab8f3ac7ba5
Yungo <29724679+yungoyungo@users.noreply.github.com>
2025-02-23 09:30:45 +0900

    fix: GAタグ修正

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,13 +9,14 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-EBJ2Q74TDC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-EBJ2Q74TDC');
-        </script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EBJ2Q74TDC');
+          `,
+        }} />
       </head>
       <body class="bg-gray-700 font-Dot text-gray-100">
         <Component />
