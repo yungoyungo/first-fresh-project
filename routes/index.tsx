255228688a425145b5aba0d222a4187e323ac8c2
Yungo <29724679+yungoyungo@users.noreply.github.com>
2025-09-29 20:52:23 +0900

    コンテンツ変更

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,15 +9,15 @@ export default function Home() {
         <MahjongTile />
       </div>
       <div class="py-4 px-4 flex flex-col items-center justify-center space-y-2">
-        <Link url="https://x.com/sslloowwllyy" text="キュウリンゴ垢" pColor="blue-300" />
+        <Link url="https://x.com/sslloowwllyy" text="本垢" pColor="blue-300" />
         <Link url="https://x.com/pasocon_suko" text="ｗｅｂ屋垢" pColor="teal-300" />
         <Link url="https://x.com/slow_slow_down_" text="車垢" pColor="orang-400" />
-        <Link url="https://x.com/z_sh_rc" text="ゲーム垢" pColor="blue-300" />
+        {/* <Link url="https://x.com/z_sh_rc" text="ゲーム垢" pColor="blue-300" /> */}
         <Link url="https://x.com/slow_slow_shot" text="写真垢" pColor="blue-300" />
         <Link url="https://note.com/sslloowwllyy/" text="note" pColor="blue-300" />
         <Link url="https://photohito.com/user/photo/166054/" text="photohito" pColor="blue-300" />
         <Link url="https://www.cizucu.com/slow_slow_down_" text="cizucu" pColor="black" />
-        <Link url="https://open.spotify.com/playlist/1ccRI6DwKUiXnanK6A5WXO?si=66bcdd1528d8411e" text="Spotifyプレイリスト" pColor="blue-300" />
+        <Link url="https://open.spotify.com/playlist/1ccRI6DwKUiXnanK6A5WXO?si=66bcdd1528d8411e" text="でえすき" pColor="blue-300" />
       </div>
     </div>
   );
